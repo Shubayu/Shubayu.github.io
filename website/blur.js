3756bb751c114a8a118f26d1607264a11d87c9f2
Shubayu <122921600+Shubayu@users.noreply.github.com>
2024-08-20 20:38:20 +0200

    Delete website directory

--- website/blur.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let cursorBlur = document.querySelector(".cursor-blur");
-
-    window.addEventListener("mousemove", function (event) {
-        cursorBlur.style.left = event.clientX + "px";
-        cursorBlur.style.top = event.clientY + "px";
-        });
-    });
